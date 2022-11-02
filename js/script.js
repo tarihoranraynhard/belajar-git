f0055ba01b0e3f49dab90749604e9ba2b0ffdb9f
tarihoranraynhard <raynhardtarihoran11@gmail.com>
2022-11-02 08:28:50 +0700

    Tutorial Git

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//DOM Tree artinya mempunyai cabang
+console.log(document);
+
+const head = document.head;
+
+const body = document.body;
+
+//Slider
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
